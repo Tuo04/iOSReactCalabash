a244e666b6c55f125835a73600a6e56848cd918a
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-06-30 17:57:15 +0800

    Test

--- index.ios.js
+++ index.ios.js
@@ -9,7 +9,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 export default class test extends Component {
@@ -19,16 +20,17 @@ export default class test extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button
+        title="Crash Button"
+        onPress={onCrash}
+        />
       </View>
     );
   }
+}
+
+function onCrash(){
+  throw new Error("this is a test JavaScript Crash !!!");
 }
 
 const styles = StyleSheet.create({
